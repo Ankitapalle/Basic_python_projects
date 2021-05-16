ab348eb73557e455fe0212a5825d4a2940cff336
Ankitapalle <cs19.ankitapalle@svceindore.ac.in>
2021-05-16 18:13:49 +0530

    Basic of Python by w3resource

---
+++ print_doc.py
@@ -0,0 +1,6 @@
+"""Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s)."""
+
+print(print.__doc__)
+print(sum.__doc__)
+print(max.__doc__)
+print(min.__doc__)
